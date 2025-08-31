8e5f5ce78af44e6b0acc4774ecf42f0ab802b1b4
Your Name <your@email.com>
2025-08-31 17:26:10 +0530

    first C program

---
+++ Hello.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;
+
+    // Input two numbers
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    // Add the numbers
+    sum = a + b;
+
+    // Output the result
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
+
